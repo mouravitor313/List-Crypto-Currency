25657a4b6c9e4d737a4e010bd032a0d3baae5ed5
mouravitor313 <mouravitor0104@gmail.com>
2025-05-06 18:01:53 -0300

    :bug:fix: Corrigir falta de atualização de preços ao inserir uma currency na execução do serviço WebSocket

--- internal/server/update.go
+++ internal/server/update.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "time"
     "github.com/mouravitor313/List-Crypto-Currency/internal/api"
+    "github.com/mouravitor313/List-Crypto-Currency/internal/models"
 )
 
 
@@ -31,9 +32,29 @@ func UpdateCryptosPeriodically() {
 func BroadcastUpdates() {
     for {
         data := <-broadcast
+
         mu.Lock()
         for client := range clients {
-            err := client.WriteJSON(data)
+            currency := client.RemoteAddr().String()
+
+            exchangeRate, err := api.GetExchangeRate(currency)
+            if err != nil {
+                fmt.Println("Erro ao obter taxa de câmbio:", err)
+                exchangeRate = 1.0
+            }
+
+            var convertedCryptos []models.Crypto
+            for _, crypto := range data {
+                convertedCryptos = append(convertedCryptos, models.Crypto{
+                    Name:          crypto.Name,
+                    Symbol:        crypto.Symbol,
+                    MarketCap:     crypto.MarketCap * exchangeRate,
+                    CurrentPrice:  crypto.CurrentPrice * exchangeRate,
+                    MarketCapRank: crypto.MarketCapRank,
+                })
+            }
+
+            err = client.WriteJSON(convertedCryptos)
             if err != nil {
                 fmt.Println("Erro ao enviar atualização:", err)
                 client.Close()
