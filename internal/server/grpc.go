c61342d6a31d734979a18dddf8831424ff303232
mouravitor313 <mouravitor0104@gmail.com>
2025-05-06 15:37:04 -0300

    :sparkles:build: Criar serviço gRPC

---
+++ internal/server/grpc.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+    "context"
+    "time"
+
+    "github.com/mouravitor313/List-Crypto-Currency/internal/api"
+    "github.com/mouravitor313/List-Crypto-Currency/internal/proto"
+)
+
+type CryptoServer struct {
+    proto.UnimplementedCryptoServiceServer
+}
+
+func (s *CryptoServer) GetTopCryptos(ctx context.Context, req *proto.CryptoRequest) (*proto.CryptoResponse, error) {
+    cryptos, err := api.GetTopCryptos()
+    if err != nil {
+        return nil, err
+    }
+
+    var response proto.CryptoResponse
+    for _, crypto := range cryptos {
+        response.Cryptos = append(response.Cryptos, &proto.Crypto{
+            Name:          crypto.Name,
+            Symbol:        crypto.Symbol,
+            MarketCap:     crypto.MarketCap,
+            CurrentPrice:  crypto.CurrentPrice,
+            MarketCapRank: int32(crypto.MarketCapRank),
+        })
+    }
+
+    return &response, nil
+}
+
+func (s *CryptoServer) StreamCryptoUpdates(req *proto.CryptoRequest, stream proto.CryptoService_StreamCryptoUpdateServer) error {
+    ticker := time.NewTicker(1 * time.Minute)
+    defer ticker.Stop()
+
+    for range ticker.C {
+        cryptos, err := api.GetTopCryptos()
+        if err != nil {
+            return err
+        }
+
+        var response proto.CryptoResponse
+        for _, crypto := range cryptos {
+            response.Cryptos = append(response.Cryptos, &proto.Crypto{
+                Name:          crypto.Name,
+                Symbol:        crypto.Symbol,
+                MarketCap:     crypto.MarketCap,
+                CurrentPrice:  crypto.CurrentPrice,
+                MarketCapRank: int32(crypto.MarketCapRank),
+            })
+        }
+
+        if err := stream.Send(&response); err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
--- main.go
+++ main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
-    "log"
+	"log"
+	"net"
 	"net/http"
 
-	"github.com/mouravitor313/List-Crypto-Currency/internal/service"
 	"github.com/mouravitor313/List-Crypto-Currency/internal/config"
 	"github.com/mouravitor313/List-Crypto-Currency/internal/models"
+	"github.com/mouravitor313/List-Crypto-Currency/internal/proto"
 	"github.com/mouravitor313/List-Crypto-Currency/internal/server"
+	"github.com/mouravitor313/List-Crypto-Currency/internal/service"
+	"google.golang.org/grpc"
+    "google.golang.org/grpc/reflection"
 )
 
 var cryptos []models.Crypto
 
 func main() {
+
+    var err error
+    var lis net.Listener
+
+    lis, err = net.Listen("tcp", ":50051")
+    if err != nil {
+        log.Fatalf("Fail to listen at: %v", err)
+    }
+
+    grpcServer := grpc.NewServer()
+    proto.RegisterCryptoServiceServer(grpcServer, &server.CryptoServer{})
+
+    reflection.Register(grpcServer)
+
+    go func() {
+        log.Println("Server gRPC running at: 50051")
+        if err := grpcServer.Serve(lis); err != nil {
+            log.Fatalf("Fail to serve: %v", err)
+        }
+    }()
+
     config.LoadAPIKey()
     if err := config.LoadAPIKey(); err != nil {
         log.Fatalf("Load API key fail: %v", err)
@@ -24,7 +49,6 @@ func main() {
         log.Fatalf("Fail to init Redis: %v", err)
     }
 
-    var err error
     cryptos, err = service.FetchTopCryptos()
     if err != nil {
         fmt.Println("Erro ao obter criptos:", err)
